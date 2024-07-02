988203fa86cbbf9a2afe1e355d117bbe12c090fc
Nurlan <001nurlan@gmail.com>
2024-07-02 11:53:45 +0500

    3. Создать модель User [#999]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -12,4 +12,16 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
 }
