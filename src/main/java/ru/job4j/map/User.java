76cf45b3973c44830c455c898dc1fb4606d6200b
Nurlan <001nurlan@gmail.com>
2024-07-05 08:19:19 +0500

    6. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -28,7 +28,7 @@ public class User {
         return birthday;
     }
 
-    /*@Override
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -38,12 +38,12 @@ public class User {
         }
         User user = (User) o;
         return Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
-    }*/
+    }
 
-    @Override
+    /*@Override
     public int hashCode() {
         return Objects.hash(name, birthday);
-    }
+    }*/
 
     public static void main(String[] args) {
         Calendar birthday = Calendar.getInstance();
