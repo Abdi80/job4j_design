9019d83b6e72204c3c8309de04652ba296780911
Nurlan <001nurlan@gmail.com>
2025-01-22 10:35:21 +0500

    0. Что такое база данных. [#6860]

---
+++ db/script.sql
@@ -0,0 +1,5 @@
+insert into apartments(home, floor, area, rooms) values('East river 5', 4, 100.5, 3);
+update apartments set home = 'West river 5';
+select * from apartments;
+delete from apartments;
+select * from apartments;
